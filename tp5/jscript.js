54c4ec461d298f5d87121f271a6b3557fbec18de
youserssif <youceff.ziraoui@gmail.com>
2017-01-09 14:22:30 +0100

    Add files via upload

--- tp5/jscript.js
+++ tp5/jscript.js
@@ -1,51 +1,54 @@
-window.onload = function(){
-    document.getElementById("searchCity").addEventListener("submit", function( event ){
+window.onload = function () {
+    document.getElementById("searchCity").addEventListener("submit", function (event) {
         event.preventDefault(); // pour annuler le rechargement de la page
         var city = document.getElementById("city").value;
-        searchCity(city);
 
+        searchCity(city);
+          });
         document.getElementById("btn_gps").addEventListener("click", function getLocation() {
 
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(searchLatLng, showError);
+               
             } else {
                 document.getElementById("maps").innerHTML = "Geolocation is not supported by this browser.";
             }
- 
+
         });
 
-    });
+  
 }
 
 
 function searchCity(_city) {
     //console.log('searchCity', 'Hello from ' + _city);
-    //A compl�ter dans la suite du TP
+    //A complet dans la suite du TP
 
     var request = new XMLHttpRequest();
-    request.open('GET', ' https://demo.bilelz.fr/owmap/?q='+_city +'&appid=0ada432b59deb9716c357092c5f79be6', true);
-   
+    request.open('GET', ' https://demo.bilelz.fr/owmap/?q=' + _city + '&appid=0ada432b59deb9716c357092c5f79be6', true);
+
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
             // Success!
             var responseJSON = JSON.parse(request.responseText);
-           // console.log(responseJSON);
+            // console.log(responseJSON);
             var date = new Date();
-            var s=date.getMinutes();
+            var s = date.getMinutes();
             var h = date.getHours();
             var icons = responseJSON.weather[0].icon;
-            var temp = responseJSON.main.temp;
+            var temp = (responseJSON.main.temp - 273).toFixed(0);
             var humidity = responseJSON.main.humidity;
             var cloud = responseJSON.clouds.all;
             var wind = responseJSON.wind.speed;
             document.getElementById("nom_w").innerHTML = responseJSON.name;
-            document.getElementById("heures").innerHTML =     "@"+h+":"+s;
-            document.getElementById("icons").innerHTML ="<img src='"+icons+".png'/>";
-            document.getElementById("temps").innerHTML = temp+"K";
-            document.getElementById("humide").innerHTML ="<b>humide </b> "+humidity + "%";
-            document.getElementById("nuages").innerHTML = "<b>Nuage    </b>     " +cloud+"%";
-            document.getElementById("vents").innerHTML = "<b>Vent      </b>     " +wind+"m/s";
-
+            document.getElementById("heures").innerHTML = "@" + h + ":" + s;
+            document.getElementById("icons").innerHTML = "<img src='" + icons + ".png'/>";
+            document.getElementById("temps").innerHTML = temp + "°";
+            document.getElementById("humide").innerHTML = "<b>humide </b> " + humidity + "%";
+            document.getElementById("nuages").innerHTML = "<b>Nuage    </b>     " + cloud + "%";
+            document.getElementById("vents").innerHTML = "<b>Vent      </b>     " + wind + "m/s";
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + _city + "&zoom=14&size=400x300&sensor=false";
+    document.getElementById("maps").innerHTML = "<img src='" + img_url + "'> ";
         } else {
             alert("Erreur de téléchargement");
         }
@@ -58,12 +61,50 @@ function searchCity(_city) {
 }
 
 
+
+
+
+
+
+
 function searchLatLng(_lat) {
     var latlon = _lat.coords.latitude + ',' + _lat.coords.longitude;
 
-    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="+latlon+"&zoom=14&size=400x300&sensor=false";
+
+
+    var request = new XMLHttpRequest();
+    request.open('GET', 'http://api.openweathermap.org/data/2.5/weather?lat='+_lat.coords.latitude+'&lon=' +_lat.coords.longitude+ '&units=metric&appid=0bb5f32378f4c71e43767c7fad870bd8', true);
+
+    request.onload = function () {
+        if (request.status >= 200 && request.status < 400) {
+            // Success!
+            var responseJSON = JSON.parse(request.responseText);
+            // console.log(responseJSON);
+            var date = new Date();
+            var s = date.getMinutes();
+            var h = date.getHours();
+            var icons = responseJSON.weather[0].icon;
+            var temp = (responseJSON.main.temp - 273).toFixed(0);
+            var humidity = responseJSON.main.humidity;
+            var cloud = responseJSON.clouds.all;
+            var wind = responseJSON.wind.speed;
+            document.getElementById("nom_w").innerHTML = responseJSON.name;
+            document.getElementById("heures").innerHTML = "@" + h + ":" + s;
+            document.getElementById("icons").innerHTML = "<img src='" + icons + ".png'/>";
+            document.getElementById("temps").innerHTML = temp + "°";
+            document.getElementById("humide").innerHTML = "<b>humide </b> " + humidity + "%";
+            document.getElementById("nuages").innerHTML = "<b>Nuage    </b>     " + cloud + "%";
+            document.getElementById("vents").innerHTML = "<b>Vent      </b>     " + wind + "m/s";
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center=" + latlon + "&zoom=14&size=400x300&sensor=false";
     document.getElementById("maps").innerHTML = "<img src='" + img_url + "'> ";
-    //A compl�ter dans la suite du TP
+        } else {
+            alert("Erreur de téléchargement");
+        }
+    };
+
+    request.onerror = function () {  };
+
+    request.send();
 }
 function showError(error) {
     switch (error.code) {
